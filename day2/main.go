15a7aea0cb26ac834e9581ae3fbaeb59ad2de41b
Charles Guertin <charlesguertin@live.ca>
2022-12-03 17:17:57 -0500

    Add day 2 part 2.

--- day2/main.go
+++ day2/main.go
@@ -10,6 +10,20 @@ import (
 var input string
 
 const (
+	theirRockShape     string = "A"
+	theirPaperShape    string = "B"
+	theirScissorsShape string = "C"
+
+	myRockShape     string = "X"
+	myPaperShape    string = "Y"
+	myScissorsShape string = "Z"
+
+	needToWin  string = myScissorsShape
+	needToDraw string = myPaperShape
+	needToLose string = myRockShape
+)
+
+const (
 	rockPoints int = 1 + iota
 	paperPoints
 	scissorsPoints
@@ -21,16 +35,24 @@ const (
 func main() {
 	lines := strings.Split(input, "\n")
 
-	scores := make([]int, len(lines))
+	scores1 := make([]int, len(lines))
+	scores2 := make([]int, len(lines))
 	for n, line := range lines {
 		splitted := strings.Split(line, " ")
 		opponentsMove := splitted[0]
 		myMove := splitted[1]
 
-		myScore := findOutcomeOfRound(myMove, opponentsMove)
-		scores[n] = myScore
+		myScore1 := findOutcomeOfRound(myMove, opponentsMove)
+		myScore2 := findOpiniatedOutcomeOfRound(myMove, opponentsMove)
+		scores1[n] = myScore1
+		scores2[n] = myScore2
 	}
 
+	printTotalScore(scores1)
+	printTotalScore(scores2)
+}
+
+func printTotalScore(scores []int) {
 	totalScore := 0
 	for _, pts := range scores {
 		totalScore += pts
@@ -38,6 +60,35 @@ func main() {
 	fmt.Println(totalScore)
 }
 
+// part 2
+func findOpiniatedOutcomeOfRound(whatINeedToDo, opponentsMove string) (myScore int) {
+	myMove := ""
+
+	switch whatINeedToDo {
+	case needToWin:
+		if opponentsMove == theirRockShape {
+			myMove = myPaperShape
+		} else if opponentsMove == theirPaperShape {
+			myMove = myScissorsShape
+		} else if opponentsMove == theirScissorsShape {
+			myMove = myRockShape
+		}
+	case needToLose:
+		if opponentsMove == theirRockShape {
+			myMove = myScissorsShape
+		} else if opponentsMove == theirPaperShape {
+			myMove = myRockShape
+		} else if opponentsMove == theirScissorsShape {
+			myMove = myPaperShape
+		}
+	case needToDraw:
+		myMove = opponentsMove
+	}
+
+	return findOutcomeOfRound(myMove, opponentsMove)
+}
+
+// part 1
 func findOutcomeOfRound(myMove, opponentsMove string) (myScore int) {
 	myScore = calculateShapeScore(myMove)
 	opponentsScore := calculateShapeScore(opponentsMove)
@@ -63,13 +114,13 @@ func findOutcomeOfRound(myMove, opponentsMove string) (myScore int) {
 func calculateShapeScore(move string) int {
 	switch move {
 	// Rock calculations
-	case "A", "X":
+	case theirRockShape, myRockShape:
 		return rockPoints
 	// Paper calculations
-	case "B", "Y":
+	case theirPaperShape, myPaperShape:
 		return paperPoints
 	// Scissors calculations
-	case "C", "Z":
+	case theirScissorsShape, myScissorsShape:
 		return scissorsPoints
 	default:
 		return 0
